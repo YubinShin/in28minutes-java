ba96b1dbc6c41021712be519184662ea9f380473
yubin thinkpad <shin.yubin18@gmail.com>
2023-10-17 18:39:45 +0900

    06 : 메서드 오버로딩으로 구구단 출력하기

---
+++ FirstJavaProjectWithIn28Minutes/src/com/in28minutes/firstjavaproject/MutiplicaitonTableRunner.java
@@ -0,0 +1,10 @@
+package com.in28minutes.firstjavaproject;
+
+public class MutiplicaitonTableRunner {
+    public static void main(String[] args) {
+        MultiplicationTable table = new MultiplicationTable();
+        // table.print();
+        // table.print(3);
+        table.print(3, 1, 2);
+    }
+}
