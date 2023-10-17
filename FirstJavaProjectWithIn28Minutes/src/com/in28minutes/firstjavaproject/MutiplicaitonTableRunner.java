f4c8ac6ffdac69e3c13c7b089f852d165362541a
yubin thinkpad <shin.yubin18@gmail.com>
2023-10-17 19:17:58 +0900

    06 : 이클립스 키보드 단축키, 변수선언시 주의점 추가

--- FirstJavaProjectWithIn28Minutes/src/com/in28minutes/firstjavaproject/MutiplicaitonTableRunner.java
+++ FirstJavaProjectWithIn28Minutes/src/com/in28minutes/firstjavaproject/MutiplicaitonTableRunner.java
@@ -4,8 +4,8 @@ public class MutiplicaitonTableRunner {
 
   public static void main(String[] args) {
     MultiplicationTable table = new MultiplicationTable();
-    // table.print();
+    table.print();
     // table.print(3);
-    table.print(3, 1, 2);
+    // table.print(3, 1, 2);
   }
 }
