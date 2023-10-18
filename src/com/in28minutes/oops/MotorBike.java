c866547405f80e6ee49019684bde457db46bbece
yubin thinkpad <shin.yubin18@gmail.com>
2023-10-18 15:29:02 +0900

    08 : 캡슐화 레벨1 : setter 를 쓰면 원하지 않는 방식으로 변수가 조작되는 것을 방지 할 수 있습니다.

--- src/com/in28minutes/oops/MotorBike.java
+++ src/com/in28minutes/oops/MotorBike.java
@@ -3,7 +3,10 @@ package com.in28minutes.oops;
 public class MotorBike {
 
   // state
-  private int speed; // member variable 는 객체에 종속됩니다. this.speed 라고 부르게됨
+  private int speed;
+
+  // member variable 는 객체에 종속됩니다. this.speed 라고 부르게됨
+  // int 멤버 변수의 디폴트 시작값은 0 입니다.
 
   // behaviour
   void start() {
@@ -17,6 +20,8 @@ public class MotorBike {
   public void setSpeed(int speed) {
     // 매개변수에 있는 speed 는 지역변수
     // 멤버변수는 this.speed
-    this.speed = speed;
+
+    // 캡슐화 LEVE1 : Setter 를 쓰면 원하지 않는 방식으로 변수가 조작되는 것을 방지 할 수 있습니다.
+    if (speed > 0) this.speed = speed;
   }
 }
