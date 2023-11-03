152ad6ed5c87cd97f3c493e03c6247be67a1b5ee
yubin thinkpad <shin.yubin18@gmail.com>
2023-11-03 13:58:16 +0900

    18 : 배열은 수정하기가 매우 어렵다. ArrayList 의 필요성

--- ArrayAndArrayList/src/com/in28minutes/arrays/Student.java
+++ ArrayAndArrayList/src/com/in28minutes/arrays/Student.java
@@ -61,7 +61,21 @@ public class Student {
     return num1.divide(num2, 3, RoundingMode.UP); // 허거덩 근데 자바9 부터 Deprecated 됐다고 한다.
   }
 
-  public void addMark(int i) {}
+  // 배열은 한번 생성하고 나면 크기가 처음에 정해진다. 이 크기를 변경할 수 없다.
+  // 유일한 옵션은 원래 요소들을 복사한 뒤에 새로운 배열을 만드는 것이다.
+  // 배열에 요소를 추가하거나 삭제하는 건 매우매우 어려운 일이다.
+  // 자바에 ArrayList라고 하는게 있는데 이게 조금 도움이 될 수도 있습니다
+  // ArrayList에 대한 소개는 다음 시간에 해보도록 할게요
+  // 본 영상에서는 배열의 길이를 직접 늘이거나 줄일 수 없다는 문제에 대해 이해하는 시간을 가졌고,
+  // 다음 영상에서는 ArrayList에 집중해 보겠습니다
 
-  public void removeMarkAtIndex(int i) {}
+  public void addMark(int i) {
+    int[] newMarks = new int[marks.length + 1];
+    marks = newMarks;
+  }
+
+  public void removeMarkAtIndex(int i) {
+    int[] newMarks = new int[marks.length - 1];
+    marks = newMarks;
+  }
 }
